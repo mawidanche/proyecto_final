8951725ce5972d49b6a2ad432d9fd79070629d1d
mawidanche <33699892+mawidanche@users.noreply.github.com>
2023-09-06 23:22:33 -0300

    init

---
+++ src/test/java/tech/escalab/spring_boot/proyecto_final/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.escalab.spring_boot.proyecto_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
